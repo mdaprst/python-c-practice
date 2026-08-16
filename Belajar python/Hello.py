edb3bacbdc6db5e89ed6f40c26ba69715e9d0b77
Mulyono Prasetyo <siapaini220105@gmail.com>
2026-08-16 17:17:56 +0900

    add hello world

--- Belajar python/Hello.py
+++ Belajar python/Hello.py
@@ -1,1 +1,36 @@
-git IndentationError
+
+# ini adalah coment, coment tidak dijalankan di python
+# print = menampilkan teks ke layar
+print("halo, dunia!")
+print("aku sedang belajar python untuk pertamakalinya")
+
+#variabel = tempan menyimpan data
+nama = "tio"
+umur = 20
+tinggi = 176.5
+is_pelajar = True
+kota = "Palembang"
+
+
+print(nama)
+print(umur)
+print(tinggi)
+print(is_pelajar)
+print(kota)
+
+
+#bisa juga di gabung
+print("nama saya:", nama)
+print("umur saya:", umur, "tahun")
+
+# === OPERASI MATEMATIKA ===
+a = 10
+b = 8
+
+print(a + b) # penjumlahan
+print(a - b) # pengurangan
+print(a * b) # perkalian
+print(a / b) # pembagian
+print(a // b) #pembagian bulat
+print(a ** b) #pangkat
+print(a % b) #sisa bagi
